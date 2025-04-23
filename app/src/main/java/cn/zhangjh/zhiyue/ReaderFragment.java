622bc92eeeddb184e5747806fcd219ddc18d028f
zhangjh <njhxzhangjihong@126.com>
2025-04-24 00:36:33 +0800

    reader

--- app/src/main/java/cn/zhangjh/zhiyue/ReaderFragment.java
+++ app/src/main/java/cn/zhangjh/zhiyue/ReaderFragment.java
@@ -1,10 +1,18 @@
 package cn.zhangjh.zhiyue;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,35 +22,79 @@ import cn.zhangjh.zhiyue.activity.MainActivity;
 
 public class ReaderFragment extends Fragment {
 
-    private String bookId;
+    private String bookUrl;
+    private WebView webView;
+    private ProgressBar progressBar;
     private boolean isNavigationVisible = false;
+    private int currentProgress = 0;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            bookId = getArguments().getString("book_id");
+            bookUrl = getArguments().getString("book_url");
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reader, container, false);
-        // 在这里添加你的 "阅读器" 页面的内容
-        TextView textView = view.findViewById(R.id.text_reader);
-        textView.setText("这里是阅读器页面");
+        
+        webView = view.findViewById(R.id.webview_reader);
+        progressBar = view.findViewById(R.id.progress_bar);
+
+        // 配置WebView
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+
+        // 添加JavaScript接口
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+
+        // 设置WebViewClient
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 页面加载完成后，调用JavaScript方法加载电子书
+                if (bookUrl != null) {
+                    webView.evaluateJavascript("loadBook('" + bookUrl + "')", null);
+                }
+            }
+        });
+
+        // 设置WebChromeClient来处理进度
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+        });
+
+        // 加载epub.js和电子书
+        String readerHtml = "file:///android_asset/reader.html";
+        webView.loadUrl(readerHtml);
 
         // 设置点击监听，切换导航栏显示状态
         view.setOnClickListener(v -> toggleNavigationVisibility());
 
         return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        // TODO: 使用bookId加载书籍内容
     }
 
     private void toggleNavigationVisibility() {
@@ -59,10 +111,28 @@ public class ReaderFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
+        if (webView != null) {
+            webView.destroy();
+        }
         super.onDestroyView();
         // 当阅读器页面销毁时，显示底部导航栏
         if (getActivity() instanceof MainActivity) {
             ((MainActivity) getActivity()).showBottomNavigation();
+        }
+    }
+
+    // JavaScript接口类
+    private class WebAppInterface {
+        @JavascriptInterface
+        public void onProgressUpdate(int progress) {
+            if (getActivity() != null) {
+                getActivity().runOnUiThread(() -> {
+                    currentProgress = progress;
+                    // 这里可以保存进度到本地存储
+                    // 也可以更新UI显示当前进度
+                    Toast.makeText(getContext(), "阅读进度: " + progress + "%", Toast.LENGTH_SHORT).show();
+                });
+            }
         }
     }
 }
