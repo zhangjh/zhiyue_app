6f633f4f7655a4e56eef4c0a73d6a23f36017716
zhangjh <njhxzhangjihong@126.com>
2025-05-28 00:24:27 +0800

    fix

--- app/src/main/java/cn/zhangjh/zhiyue/utils/LogUtil.java
+++ app/src/main/java/cn/zhangjh/zhiyue/utils/LogUtil.java
@@ -10,19 +10,19 @@ public class LogUtil {
 
     public static void d(@NonNull String tag, @NonNull String message) {
         if(BuildConfig.DEBUG) {
-            LogUtil.d(tag, message);
+            Log.d(tag, message);
         }
     }
 
     public static void e(@NonNull String tag, @NonNull String message) {
         if(BuildConfig.DEBUG) {
-            LogUtil.e(tag, message);
+            Log.e(tag, message);
         }
     }
 
     public static void e(@NonNull String tag, @NonNull String message, @NonNull Throwable t) {
         if(BuildConfig.DEBUG) {
-            LogUtil.e(tag, message, t);
+            Log.e(tag, message, t);
         }
     }
 }
